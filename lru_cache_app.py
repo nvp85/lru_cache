bef2922ba1c4bc61b51a03005957f283f602a6fa
nvp <nvp@zhukov.al>
2019-01-16 01:02:18 -0800

    initial commit

---
+++ lru_cache_app.py
@@ -0,0 +1,48 @@
+import sys
+from collections import OrderedDict
+
+
+def lru_cache(input=sys.stdin, output=sys.stdout):
+    size = None
+    cache = OrderedDict()
+    while not size:
+        size_input = input.readline().strip().split(' ')
+        if size_input[0] == 'SIZE' and len(size_input) == 2:
+            try:
+                size = int(size_input[1])
+                output.write('SIZE OK\n')
+            except (TypeError, ValueError):
+                output.write('Size must be integer\n')
+        else:
+            output.write('You should set size of a cache:\n')
+    for s in input:
+        s = s.strip().split(' ')
+        if len(s) > 0:
+            command = s[0]
+            if command == 'EXIT':
+                break
+            elif command == 'SET':
+                if len(s) == 3:
+                    key, value = s[1], s[2]
+                    cache[key] = value
+                    output.write('SET OK\n')
+                    if len(cache) > 3:
+                        cache.popitem(last=False)
+                else:
+                    output.write('ERROR: invalid input.\n')
+            elif command == 'GET':
+                if len(s) == 2:
+                    key = s[1]
+                    try:
+                        value = cache.pop(key)
+                        output.write('GOT {0}\n'.format(value))
+                        cache[key] = value
+                    except KeyError:
+                        output.write('NOTFOUND\n')
+                else:
+                    output.write('ERROR\n')
+
+
+
+if __name__ == "__main__":
+    lru_cache()
---
+++ tests.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest import mock
+from io import StringIO
+from lru_cache_app import lru_cache
+
+
+class lruTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.stdout = StringIO()
+        self.stdin = StringIO()
+
+    def test_lru_cache(self):
+        self.stdin.write('SIZE 3\nGET foo\nSET foo 1\n')
+        self.stdin.write('GET foo\nSET foo 1.1\nGET foo\n')
+        self.stdin.write('SET spam 2\nGET spam\n')
+        self.stdin.write('SET ham third\nSET parrot four\n')
+        self.stdin.write('GET foo\nGET spam\nGET ham\n')
+        self.stdin.write('GET ham parrot\nGET parrot\n')
+        self.stdin.write('EXIT\n')
+        self.stdin.seek(0)
+        lru_cache(input=self.stdin, output=self.stdout)
+        self.assertEqual(self.stdout.getvalue().strip(), """SIZE OK
+NOTFOUND
+SET OK
+GOT 1
+SET OK
+GOT 1.1
+SET OK
+GOT 2
+SET OK
+SET OK
+NOTFOUND
+GOT 2
+GOT third
+ERROR
+GOT four""")
+
+
+if __name__ == '__main__':
+    unittest.main()
